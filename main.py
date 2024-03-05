987192fc4ac085618a7853147e4fbb8b2e29f65a
Ed Mamerto <edmamerto@gmail.com>
2024-03-05 13:31:37 -0500

    Add hello world boilerplate #2 (#2)
    * Add hello world boilerplate (#1)
    
    * Ignore pycache (#1)
    
    * Add script: main, for shortcut (#1)
    
    ---------
    
    Co-authored-by: edmamerto <edgarmamerto@Edgars-MacBook-Pro.local>

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.hello import say_hello
+
+def main() -> None:  # This indicates that 'main' does not return any value.
+    print(say_hello())
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_hello.py
@@ -0,0 +1,4 @@
+from src.hello import say_hello
+
+def test_say_hello() -> None:
+    assert say_hello() == "Hello, World!"
