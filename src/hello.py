45fcf8a988f3965ae0292e6045236c25bfd1532f
Edgar Mamerto <edmamerto@gmail.com>
2024-03-05 20:16:08 -0500

    Add structlog #7

--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,11 @@
+from src.log import get_logger
 from src.models import User
+
+# name of the current module
+logger = get_logger(__name__)
 
 
 def greet_user() -> str:
     user = User(id=1, name="John Doe", email="john.doe@example.com")
+    logger.debug("Greeting user", user_id=user.id, user_name=user.name)
     return f"Hello, {user.name}!"
